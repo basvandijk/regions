07a96f1eadec669dcd637a61b3fab407b812b73c
Bas van Dijk <v.dijk.bas@gmail.com>
2010-04-12 23:59:21 +0200

    Renamed openResource and closeResource to open and close respectively
    Ignore-this: cd9515464ed87cbec87055a6c6212c6c
    The intention is that Control.Resource should be imported qualified
    Bumped version from 0.4.0.1 to 0.5 because of this API change
    
    darcs-hash:20100412215921-ae560-f8b57b8d4d43f3737d2144d8ad5493504528e199.gz

--- Control/Resource.hs
+++ Control/Resource.hs
@@ -7,17 +7,19 @@
 -- License     :  BSD3 (see the file LICENSE)
 -- Maintainer  :  Bas van Dijk <v.dijk.bas@gmail.com>
 --
--- Unsafely opening and closing of scarce resources.
+-- /Unsafely/ opening and closing of scarce resources.
 --
 --------------------------------------------------------------------------------
 
 module Control.Resource
     ( Resource
     , Handle
-    , openResource
-    , closeResource
+    , open
+    , close
     ) where
 
+
+-- from base:
 import System.IO ( IO )
 
 {-| Class of /scarce/ resources. A scarce resource is a resource that only one
@@ -31,17 +33,19 @@ sole access to the resource. When the resource is no longer needed it should be
 class Resource resource where
     data Handle resource ∷ *
 
-    -- | Yield an @IO@ computation that opens the given resource and returns a
-    -- @Handle@ on it.
-    --
-    -- Think of a handle as your private session with the resource. As long as
-    -- you have it, other users can't use the resource. So don't forget to close
-    -- this session with the resource as soon as you're done with it.
-    openResource  ∷ resource → IO (Handle resource)
+    {-| Yield an @IO@ computation that opens the given resource and returns a
+    @Handle@ on it.
 
-    -- | Yield an @IO@ computation that closes the resource identified by the
-    -- given @Handle@.
-    --
-    -- /Warning/: Using the @Handle@ after the resource has been closed will
-    -- usually throw an exception or result in a program crash!
-    closeResource ∷ Handle resource → IO ()
+    Think of a handle as your private session with the resource. As long as
+    you have it, other users can't use the resource. So don't forget to close
+    this session with the resource as soon as you're done with it.
+    -}
+    open ∷ resource → IO (Handle resource)
+
+    {-| Yield an @IO@ computation that closes the resource identified by the
+    given @Handle@.
+
+    /Warning/: Using the @Handle@ after the resource has been closed will
+    usually throw an exception or result in a program crash!
+    -}
+    close ∷ Handle resource → IO ()
