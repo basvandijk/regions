8248a56d7ad23d9b5308cf14cfd54b073126a758
Bas van Dijk <v.dijk.bas@gmail.com>
2010-06-08 10:12:19 +0200

    Updated copyrights
    Ignore-this: 95ff1984aa2fda2ede88c1ffe203d185
    
    darcs-hash:20100608081219-ae560-ae5b0c922d6d09bf802053953b0218fa68818bc8.gz

--- Control/Monad/Trans/Region.hs
+++ Control/Monad/Trans/Region.hs
@@ -2,7 +2,7 @@
 -------------------------------------------------------------------------------
 -- |
 -- Module      :  Control.Monad.Trans.Region
--- Copyright   :  (c) 2009 Bas van Dijk
+-- Copyright   :  (c) 2009-2010 Bas van Dijk
 -- License     :  BSD3 (see the file LICENSE)
 -- Maintainer  :  Bas van Dijk <v.dijk.bas@gmail.com>
 --
